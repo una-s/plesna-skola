06258fff2dd3cc33675810c481a1b6ce27fc635e
Una Stankovic <unastankovic03@gmail.com>
2026-07-30 11:56:15 +0200

    Dodati testovi za metode pristupa bazi klase Ucenik
    - Testiran uslovZaSelect (sve grane: jedno polje, vise polja sa AND,
    prazno)
    - Testirani vratiVrednostZaUbacivanje, vratiVrednostZaIzmenu,
    vratiPrimarniKljuc

--- zajednicki/src/test/java/domen/UcenikTest.java
+++ zajednicki/src/test/java/domen/UcenikTest.java
@@ -16,7 +16,7 @@ import org.junit.jupiter.params.provider.CsvSource;
  * 
  */
 class UcenikTest {
-	
+
 	Ucenik u;
 
 	/**
@@ -24,7 +24,7 @@ class UcenikTest {
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
-		u=new Ucenik();
+		u = new Ucenik();
 	}
 
 	/**
@@ -32,7 +32,7 @@ class UcenikTest {
 	 */
 	@AfterEach
 	void tearDown() throws Exception {
-		u=null;
+		u = null;
 	}
 
 	/**
@@ -40,93 +40,95 @@ class UcenikTest {
 	 */
 	@Test
 	void testHashCode() {
-	    u.setBrojTelefona("0641111111");
-	    
-	    Ucenik u2 = new Ucenik();
-	    u2.setBrojTelefona("0641111111");
-	    
-	    assertEquals(u.hashCode(), u2.hashCode());
+		u.setBrojTelefona("0641111111");
+
+		Ucenik u2 = new Ucenik();
+		u2.setBrojTelefona("0641111111");
+
+		assertEquals(u.hashCode(), u2.hashCode());
 	}
+
 	/**
 	 * Test method for {@link domen.Ucenik#Ucenik()}.
 	 */
 	@Test
-	 void testUcenik() {
+	void testUcenik() {
 		assertNotNull(u);
 	}
 
 	/**
-	 * Test method for {@link domen.Ucenik#Ucenik(java.lang.Long, java.lang.String, java.lang.String, java.lang.String, domen.PlesniNivo)}.
+	 * Test method for
+	 * {@link domen.Ucenik#Ucenik(java.lang.Long, java.lang.String, java.lang.String, java.lang.String, domen.PlesniNivo)}.
 	 */
 	@Test
-	 void testUcenikLongStringStringStringPlesniNivo() {
-		 PlesniNivo pn = new PlesniNivo();
-		    Ucenik ucenik = new Ucenik(1L, "Ivo", "Ivic", "0641234567", pn);
-		    
-		    assertEquals(1L, ucenik.getIdUcenik());
-		    assertEquals("Ivo", ucenik.getIme());
-		    assertEquals("Ivic", ucenik.getPrezime());
-		    assertEquals("0641234567", ucenik.getBrojTelefona());
-		    assertEquals(pn, ucenik.getPlesniNivo());
+	void testUcenikLongStringStringStringPlesniNivo() {
+		PlesniNivo pn = new PlesniNivo();
+		Ucenik ucenik = new Ucenik(1L, "Ivo", "Ivic", "0641234567", pn);
+
+		assertEquals(1L, ucenik.getIdUcenik());
+		assertEquals("Ivo", ucenik.getIme());
+		assertEquals("Ivic", ucenik.getPrezime());
+		assertEquals("0641234567", ucenik.getBrojTelefona());
+		assertEquals(pn, ucenik.getPlesniNivo());
 	}
 
 	/**
 	 * Test method for {@link domen.Ucenik#setIdUcenik(java.lang.Long)}.
 	 */
 	@Test
-	 void testSetIdUcenik() {
-		 u.setIdUcenik(5L);
-		 assertEquals(5L, u.getIdUcenik());
+	void testSetIdUcenik() {
+		u.setIdUcenik(5L);
+		assertEquals(5L, u.getIdUcenik());
 	}
 
 	/**
 	 * Test method for {@link domen.Ucenik#setIme(java.lang.String)}.
 	 */
 	@Test
-	 void testSetIme() {
+	void testSetIme() {
 		u.setIme("Marko");
-	    assertEquals("Marko", u.getIme());
+		assertEquals("Marko", u.getIme());
 	}
 
 	/**
 	 * Test method for {@link domen.Ucenik#setPrezime(java.lang.String)}.
 	 */
 	@Test
-	 void testSetPrezime() {
+	void testSetPrezime() {
 		u.setPrezime("Markovic");
-	    assertEquals("Markovic", u.getPrezime());
+		assertEquals("Markovic", u.getPrezime());
 	}
 
 	/**
 	 * Test method for {@link domen.Ucenik#setBrojTelefona(java.lang.String)}.
 	 */
 	@Test
-	 void testSetBrojTelefona() {
+	void testSetBrojTelefona() {
 		u.setBrojTelefona("0653334444");
-	    assertEquals("0653334444", u.getBrojTelefona());
+		assertEquals("0653334444", u.getBrojTelefona());
 	}
 
 	/**
 	 * Test method for {@link domen.Ucenik#setPlesniNivo(domen.PlesniNivo)}.
 	 */
 	@Test
-	 void testSetPlesniNivo() {
+	void testSetPlesniNivo() {
 		PlesniNivo pn = new PlesniNivo();
-	    u.setPlesniNivo(pn);
-	    assertEquals(pn, u.getPlesniNivo());
+		u.setPlesniNivo(pn);
+		assertEquals(pn, u.getPlesniNivo());
 	}
 
 	/**
 	 * Test method for {@link domen.Ucenik#toString()}.
 	 */
 	@Test
-	 void testToString() {
+	void testToString() {
 		u.setIme("Ivo");
-	    u.setPrezime("Ivic");
-	    
-	    String rezultat = u.toString();
-	    
-	    assertEquals("Ivo Ivic", rezultat);
+		u.setPrezime("Ivic");
+
+		String rezultat = u.toString();
+
+		assertEquals("Ivo Ivic", rezultat);
 	}
 
 	/**
@@ -134,32 +136,80 @@ class UcenikTest {
 	 */
 	@ParameterizedTest
 	@DisplayName("Dva ucenika su jednaka ako imaju isti broj telefona")
-	@CsvSource({
-	    "0641111111, 0641111111, true",
-	    "0641111111, 0642222222, false"
-	})
-	 void testEqualsObject(String tel1, String tel2, boolean ocekivano) {
+	@CsvSource({ "0641111111, 0641111111, true", "0641111111, 0642222222, false" })
+	void testEqualsObject(String tel1, String tel2, boolean ocekivano) {
 		u.setBrojTelefona(tel1);
-	    
-	    Ucenik u2 = new Ucenik();
-	    u2.setBrojTelefona(tel2);
-	    
-	    assertEquals(ocekivano, u.equals(u2));
+
+		Ucenik u2 = new Ucenik();
+		u2.setBrojTelefona(tel2);
+
+		assertEquals(ocekivano, u.equals(u2));
 	}
-	
+
 	@Test
 	void testEqualsIstiObjekat() {
-	    u.setBrojTelefona("0641111111");
-	    assertTrue(u.equals(u));
+		u.setBrojTelefona("0641111111");
+		assertTrue(u.equals(u));
 	}
-	
+
 	@Test
 	void testEqualsNull() {
-	    assertFalse(u.equals(null));
+		assertFalse(u.equals(null));
 	}
-	
+
 	@Test
 	void testEqualsDrugaKlasa() {
-	    assertFalse(u.equals(new PlesniNivo()));
+		assertFalse(u.equals(new PlesniNivo()));
+	}
+
+	@Test
+	void testUslovZaSelectSamoIme() {
+		u.setIme("Marko");
+		assertEquals("u.ime LIKE '%Marko%'", u.uslovZaSelect());
+	}
+
+	@Test
+	void testUslovZaSelectImeIPrezime() {
+		u.setIme("Marko");
+		u.setPrezime("Markovic");
+		assertEquals("u.ime LIKE '%Marko%' AND u.prezime LIKE '%Markovic%'", u.uslovZaSelect());
+	}
+
+	@Test
+	void testUslovZaSelectSvaPolja() {
+		PlesniNivo pn = new PlesniNivo(2L, "Srednji", 2);
+		u.setIme("Marko");
+		u.setPrezime("Markovic");
+		u.setBrojTelefona("0641112223");
+		u.setPlesniNivo(pn);
+		assertEquals(
+				"u.ime LIKE '%Marko%' AND u.prezime LIKE '%Markovic%' AND u.idPlesniNivo = 2 AND u.brojTelefona = '0641112223'",
+				u.uslovZaSelect());
+	}
+
+	@Test
+	void testUslovZaSelectPrazno() {
+		assertEquals("", u.uslovZaSelect());
+	}
+
+	@Test
+	void testVratiVrednostZaUbacivanje() {
+		PlesniNivo pn = new PlesniNivo(2L, "Srednji", 2);
+		Ucenik ucenik = new Ucenik(1L, "Marko", "Markovic", "0641112223", pn);
+		assertEquals("'Marko', 'Markovic', '0641112223', 2", ucenik.vratiVrednostZaUbacivanje());
+	}
+
+	@Test
+	void testVratiVrednostZaIzmenu() {
+		PlesniNivo pn = new PlesniNivo(2L, "Srednji", 2);
+		Ucenik ucenik = new Ucenik(1L, "Marko", "Markovic", "0641112223", pn);
+		assertEquals("ime = 'Marko', prezime = 'Markovic', brojTelefona = '0641112223', idPlesniNivo = 2",
+				ucenik.vratiVrednostZaIzmenu());
+	}
+
+	@Test
+	void testVratiPrimarniKljuc() {
+		u.setIdUcenik(5L);
+		assertEquals("idUcenik = 5", u.vratiPrimarniKljuc());
 	}
 }
