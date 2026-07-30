217acdc30fdb516fbf927cfe8c86c5c297ffe4c7
Una Stankovic <unastankovic03@gmail.com>
2026-07-30 12:46:07 +0200

    Dodati testovi za metode pristupa bazi klase Sertifikat
    - Testirani vratiVrednostZaUbacivanje, vratiVrednostZaIzmenu,
    vratiPrimarniKljuc

--- zajednicki/src/test/java/domen/SertifikatTest.java
+++ zajednicki/src/test/java/domen/SertifikatTest.java
@@ -16,7 +16,7 @@ import org.junit.jupiter.params.provider.CsvSource;
  * 
  */
 class SertifikatTest {
-	
+
 	Sertifikat s;
 
 	/**
@@ -32,110 +32,127 @@ class SertifikatTest {
 	 */
 	@AfterEach
 	void tearDown() throws Exception {
-		s=null;
+		s = null;
 	}
 
 	/**
 	 * Test method for {@link domen.Sertifikat#hashCode()}.
 	 */
 	@Test
-	 void testHashCode() {
-		 s.setInstitucija("Plesna Akademija Korak");
-	        s.setNaziv("Salsa instruktor");
+	void testHashCode() {
+		s.setInstitucija("Plesna Akademija Korak");
+		s.setNaziv("Salsa instruktor");
 
-	        Sertifikat s2 = new Sertifikat();
-	        s2.setInstitucija("Plesna Akademija Korak");
-	        s2.setNaziv("Salsa instruktor");
+		Sertifikat s2 = new Sertifikat();
+		s2.setInstitucija("Plesna Akademija Korak");
+		s2.setNaziv("Salsa instruktor");
 
-	        assertEquals(s.hashCode(), s2.hashCode());
+		assertEquals(s.hashCode(), s2.hashCode());
 	}
 
 	/**
-	 * Test method for {@link domen.Sertifikat#Sertifikat(java.lang.Long, java.lang.String, java.lang.String)}.
+	 * Test method for
+	 * {@link domen.Sertifikat#Sertifikat(java.lang.Long, java.lang.String, java.lang.String)}.
 	 */
 	@Test
-	 void testSertifikatLongStringString() {
+	void testSertifikatLongStringString() {
 		Sertifikat sert = new Sertifikat(1L, "Plesna Akademija Korak", "Salsa instruktor");
 
-        assertEquals(1L, sert.getIdSertifikat());
-        assertEquals("Plesna Akademija Korak", sert.getInstitucija());
-        assertEquals("Salsa instruktor", sert.getNaziv());
+		assertEquals(1L, sert.getIdSertifikat());
+		assertEquals("Plesna Akademija Korak", sert.getInstitucija());
+		assertEquals("Salsa instruktor", sert.getNaziv());
 	}
 
 	/**
 	 * Test method for {@link domen.Sertifikat#setIdSertifikat(java.lang.Long)}.
 	 */
 	@Test
-	 void testSetIdSertifikat() {
+	void testSetIdSertifikat() {
 		s.setIdSertifikat(5L);
-        assertEquals(5L, s.getIdSertifikat());
+		assertEquals(5L, s.getIdSertifikat());
 	}
 
 	/**
 	 * Test method for {@link domen.Sertifikat#setInstitucija(java.lang.String)}.
 	 */
 	@Test
-	 void testSetInstitucija() {
+	void testSetInstitucija() {
 		s.setInstitucija("Plesni savez Srbije");
-        assertEquals("Plesni savez Srbije", s.getInstitucija());
+		assertEquals("Plesni savez Srbije", s.getInstitucija());
 	}
 
 	/**
 	 * Test method for {@link domen.Sertifikat#setNaziv(java.lang.String)}.
 	 */
 	@Test
-	 void testSetNaziv() {
-		 s.setNaziv("Tango instruktor");
-	     assertEquals("Tango instruktor", s.getNaziv());
+	void testSetNaziv() {
+		s.setNaziv("Tango instruktor");
+		assertEquals("Tango instruktor", s.getNaziv());
 	}
 
 	/**
 	 * Test method for {@link domen.Sertifikat#equals(java.lang.Object)}.
 	 */
-    @ParameterizedTest
-    @DisplayName("Dva sertifikata su jednaka ako imaju istu instituciju i naziv")
-    @CsvSource({
-        "Plesna Akademija Korak, Salsa, Plesna Akademija Korak, Salsa, true",
-        "Plesna Akademija Korak, Salsa, Institut za Igru i Ples, Salsa, false",
-        "Institut za Igru i Ples, Salsa, Institut za Igru i Ples, Tango, false"
-    })
-    void testEqualsObject(String inst1, String naziv1, String inst2, String naziv2, boolean ocekivano) {
-        s.setInstitucija(inst1);
-        s.setNaziv(naziv1);
+	@ParameterizedTest
+	@DisplayName("Dva sertifikata su jednaka ako imaju istu instituciju i naziv")
+	@CsvSource({ "Plesna Akademija Korak, Salsa, Plesna Akademija Korak, Salsa, true",
+			"Plesna Akademija Korak, Salsa, Institut za Igru i Ples, Salsa, false",
+			"Institut za Igru i Ples, Salsa, Institut za Igru i Ples, Tango, false" })
+	void testEqualsObject(String inst1, String naziv1, String inst2, String naziv2, boolean ocekivano) {
+		s.setInstitucija(inst1);
+		s.setNaziv(naziv1);
 
-        Sertifikat s2 = new Sertifikat();
-        s2.setInstitucija(inst2);
-        s2.setNaziv(naziv2);
+		Sertifikat s2 = new Sertifikat();
+		s2.setInstitucija(inst2);
+		s2.setNaziv(naziv2);
 
-        assertEquals(ocekivano, s.equals(s2));
-    }
-    
-    @Test
-    void testEqualsIstiObjekat() {
-        s.setInstitucija("Plesna Akademija Korak");
-        s.setNaziv("Salsa");
-        assertTrue(s.equals(s));
-    }
+		assertEquals(ocekivano, s.equals(s2));
+	}
 
-    @Test
-    void testEqualsNull() {
-        assertFalse(s.equals(null));
-    }
+	@Test
+	void testEqualsIstiObjekat() {
+		s.setInstitucija("Plesna Akademija Korak");
+		s.setNaziv("Salsa");
+		assertTrue(s.equals(s));
+	}
 
-    @Test
-    void testEqualsDrugaKlasa() {
-        assertFalse(s.equals(new Ucenik()));
-    }
+	@Test
+	void testEqualsNull() {
+		assertFalse(s.equals(null));
+	}
+
+	@Test
+	void testEqualsDrugaKlasa() {
+		assertFalse(s.equals(new Ucenik()));
+	}
 
 	/**
 	 * Test method for {@link domen.Sertifikat#toString()}.
 	 */
 	@Test
-	 void testToString() {
+	void testToString() {
 		s.setNaziv("Salsa instruktor");
-        s.setInstitucija("FON");
+		s.setInstitucija("Akademija Savremenog Plesa");
 
-        assertEquals("Salsa instruktor FON", s.toString());
+		assertEquals("Salsa instruktor Akademija Savremenog Plesa", s.toString());
+	}
+
+	@Test
+	void testVratiVrednostZaUbacivanje() {
+		Sertifikat sert = new Sertifikat(1L, "Akademija Savremenog Plesa", "Salsa instruktor");
+		assertEquals("'Akademija Savremenog Plesa', 'Salsa instruktor'", sert.vratiVrednostZaUbacivanje());
+	}
+
+	@Test
+	void testVratiVrednostZaIzmenu() {
+		Sertifikat sert = new Sertifikat(1L, "Akademija Savremenog Plesa", "Salsa instruktor");
+		assertEquals("institucija = 'Akademija Savremenog Plesa', naziv = 'Salsa instruktor'", sert.vratiVrednostZaIzmenu());
+	}
+
+	@Test
+	void testVratiPrimarniKljuc() {
+		s.setIdSertifikat(5L);
+		assertEquals("idSertifikat = 5", s.vratiPrimarniKljuc());
 	}
 
 }
