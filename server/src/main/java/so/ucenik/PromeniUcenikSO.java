ca930dd857c7b41cfaebdb83d14e554e2057deb7
Una Stankovic <unastankovic03@gmail.com>
2026-07-31 19:03:24 +0200

    Testirana klasa PromeniUcenikSO
    - Testiran preduslov (svih 6 scenarija validacije + ispravan slucaj)
    - Testirana izvrsiOperaciju sa mockom (repository.edit preko
    expectLastCall)

--- server/src/main/java/so/ucenik/PromeniUcenikSO.java
+++ server/src/main/java/so/ucenik/PromeniUcenikSO.java
@@ -7,6 +7,7 @@ package so.ucenik;
 import domen.ApstraktniDomenskiObjekat;
 import domen.Ucenik;
 import so.ApstraktnaSO;
+import repository.Repository;
 
 /**
  *
@@ -14,32 +15,40 @@ import so.ApstraktnaSO;
  */
 public class PromeniUcenikSO extends ApstraktnaSO {
 
-    @Override
-    protected void preduslov(ApstraktniDomenskiObjekat ado) throws Exception {
-        if (ado == null) {
-            throw new Exception("Ucenik ne sme biti null!");
-        }
-        if (!(ado instanceof Ucenik)) {
-            throw new Exception("Prosledjeni objekat nije instanca klase Ucenik!");
-        }
-        Ucenik u = (Ucenik) ado;
-        if (u.getIme() == null || u.getIme().isEmpty()) {
-            throw new Exception("Ime ne sme biti prazno!");
-        }
-        if (u.getPrezime() == null || u.getPrezime().isEmpty()) {
-            throw new Exception("Prezime ne sme biti prazno!");
-        }
-        if (u.getBrojTelefona() == null || u.getBrojTelefona().isEmpty()) {
-            throw new Exception("Broj telefona ne sme biti prazan!");
-        }
-        if (u.getPlesniNivo()== null) {
-            throw new Exception("Plesni nivo ne sme biti prazan!");
-        }
-    }
+	public PromeniUcenikSO() {
+		super();
+	}
 
-    @Override
-    protected void izvrsiOperaciju(ApstraktniDomenskiObjekat ado) throws Exception {
-        Ucenik u = (Ucenik) ado;
-        repository.edit(u);
-    }
+	public PromeniUcenikSO(Repository repository) {
+		super(repository);
+	}
+
+	@Override
+	protected void preduslov(ApstraktniDomenskiObjekat ado) throws Exception {
+		if (ado == null) {
+			throw new Exception("Ucenik ne sme biti null!");
+		}
+		if (!(ado instanceof Ucenik)) {
+			throw new Exception("Prosledjeni objekat nije instanca klase Ucenik!");
+		}
+		Ucenik u = (Ucenik) ado;
+		if (u.getIme() == null || u.getIme().isEmpty()) {
+			throw new Exception("Ime ne sme biti prazno!");
+		}
+		if (u.getPrezime() == null || u.getPrezime().isEmpty()) {
+			throw new Exception("Prezime ne sme biti prazno!");
+		}
+		if (u.getBrojTelefona() == null || u.getBrojTelefona().isEmpty()) {
+			throw new Exception("Broj telefona ne sme biti prazan!");
+		}
+		if (u.getPlesniNivo() == null) {
+			throw new Exception("Plesni nivo ne sme biti prazan!");
+		}
+	}
+
+	@Override
+	protected void izvrsiOperaciju(ApstraktniDomenskiObjekat ado) throws Exception {
+		Ucenik u = (Ucenik) ado;
+		repository.edit(u);
+	}
 }
