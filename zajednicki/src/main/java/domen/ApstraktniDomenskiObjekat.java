4061e60479a3762096264b6de6b9ddb54d21c7af
Una Stankovic <unastankovic03@gmail.com>
2026-08-09 11:33:28 +0200

    Dokumentovan interfejs ApstraktniDomenskiObjekat JavaDoc-om

--- zajednicki/src/main/java/domen/ApstraktniDomenskiObjekat.java
+++ zajednicki/src/main/java/domen/ApstraktniDomenskiObjekat.java
@@ -9,30 +9,119 @@ import java.util.List;
 import java.sql.ResultSet;
 
 /**
- *
- * @author Una
+ * Predstavlja apstraktni domenski objekat koji mogu da implementiraju sve
+ * domenske klase u sistemu.
+ * 
+ * Definise metode potrebne za rad sa bazom podataka - vracanje naziva tabele,
+ * kolona i vrednosti za ubacivanje i izmenu, uslova za pretragu, kao i za
+ * kreiranje domenskih objekata iz rezultata upita (ResultSet).
+ * 
+ * @author Una Stankovic
+ * @version 1.0
  */
 public interface ApstraktniDomenskiObjekat extends Serializable {
 
-    public String vratiNazivTabele();
+	/**
+	 * Vraca naziv tabele u bazi podataka koja odgovara domenskom objektu.
+	 * 
+	 * @return Naziv tabele kao String.
+	 */
+	public String vratiNazivTabele();
 
-    public List<ApstraktniDomenskiObjekat> vratiListu(ResultSet rs) throws Exception;
+	
+	/**
+     * Kreira i vraca listu domenskih objekata na osnovu rezultata
+     * upita nad bazom podataka.
+     * 
+     * @param rs Rezultat upita (ResultSet) iz koga se citaju podaci.
+     * @return Lista domenskih objekata kreiranih iz rezultata upita.
+     * @throws java.lang.Exception ako dodje do greske pri citanju
+     * iz rezultata upita.
+     */
+	public List<ApstraktniDomenskiObjekat> vratiListu(ResultSet rs) throws Exception;
 
-    public String vratiKoloneZaUbacivanje();
+	
+	/**
+     * Vraca nazive kolona u koje se ubacuju vrednosti prilikom
+     * dodavanja novog sloga u bazu.
+     * 
+     * @return Nazivi kolona za ubacivanje, razdvojeni zarezom, kao String.
+     */
+	public String vratiKoloneZaUbacivanje();
 
-    public String vratiVrednostZaUbacivanje();
+	
+	 /**
+     * Vraca vrednosti atributa domenskog objekta formatirane za
+     * ubacivanje novog sloga u bazu.
+     * 
+     * @return Vrednosti za ubacivanje kao String.
+     */
+	public String vratiVrednostZaUbacivanje();
 
-    public String vratiPrimarniKljuc();
+	
+	/**
+     * Vraca uslov koji jedinstveno identifikuje slog u bazi na osnovu
+     * primarnog kljuca domenskog objekta.
+     * 
+     * @return Uslov sa primarnim kljucem kao String.
+     */
+	public String vratiPrimarniKljuc();
 
-    public ApstraktniDomenskiObjekat vratiObjekatIzRS(ResultSet rs) throws Exception;
+	
+	 /**
+     * Kreira i vraca jedan domenski objekat na osnovu rezultata
+     * upita nad bazom podataka.
+     * 
+     * @param rs Rezultat upita (ResultSet) iz koga se citaju podaci.
+     * @return Domenski objekat kreiran iz rezultata upita ili null
+     * ako u rezultatu nema sloga.
+     * @throws java.lang.Exception ako dodje do greske pri citanju
+     * iz rezultata upita.
+     */
+	public ApstraktniDomenskiObjekat vratiObjekatIzRS(ResultSet rs) throws Exception;
 
-    public String vratiVrednostZaIzmenu();
+	
+	/**
+     * Vraca parove kolona i vrednosti formatirane za izmenu
+     * postojeceg sloga u bazi.
+     * 
+     * @return Vrednosti za izmenu kao String.
+     */
+	public String vratiVrednostZaIzmenu();
 
-    public String join();
+	
+	/**
+     * Vraca deo SQL upita za spajanje (JOIN) sa drugim tabelama,
+     * ako je potrebno.
+     * 
+     * @return JOIN deo upita kao String ili prazan String ako
+     * spajanje nije potrebno.
+     */
+	public String join();
 
-    public String uslovZaSelect();
-    
-    public String alias();
-    
-    void postaviId(Long id);
+	
+	/**
+     * Vraca uslov za pretragu (WHERE deo upita) na osnovu popunjenih
+     * atributa domenskog objekta.
+     * 
+     * @return Uslov za pretragu kao String ili prazan String ako
+     * nijedan kriterijum nije popunjen.
+     */
+	public String uslovZaSelect();
+
+	
+	 /**
+     * Vraca alijas (skracenicu) tabele koji se koristi u SQL upitima.
+     * 
+     * @return Alijas tabele kao String.
+     */
+	public String alias();
+
+	
+	/**
+     * Postavlja vrednost primarnog kljuca (ID) domenskog objekta.
+     * 
+     * @param id Nova vrednost primarnog kljuca kao Long.
+     */
+	void postaviId(Long id);
 }
