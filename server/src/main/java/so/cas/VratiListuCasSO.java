a46c38e07670cd6cb4666d2819235deb76702f6b
Una Stankovic <unastankovic03@gmail.com>
2026-08-10 15:18:16 +0200

    Dokumentovana klasa VratiListuCasSO JavaDoc-om

--- server/src/main/java/so/cas/VratiListuCasSO.java
+++ server/src/main/java/so/cas/VratiListuCasSO.java
@@ -6,14 +6,33 @@ import java.util.List;
 import so.ApstraktnaSO;
 import repository.Repository;
 
+/**
+ * Predstavlja sistemsku operaciju kojom se vraca lista svih casova
+ * iz sistema.
+ * 
+ * Nasledjuje ApstraktnaSO i implementira preduslov i izvrsiOperaciju.
+ * 
+ * @author Una Stankovic
+ */
 public class VratiListuCasSO extends ApstraktnaSO {
 
+	/**
+	 * Lista casova vracena iz baze podataka.
+	 */
 	private List<Cas> casovi;
 
+	/**
+	 * Kreira objekat sistemske operacije sa podrazumevanim repozitorijumom.
+	 */
 	public VratiListuCasSO() {
 		super();
 	}
 
+	/**
+	 * Kreira objekat sistemske operacije sa unetim repozitorijumom.
+	 * 
+	 * @param repository Repozitorijum koji sistemska operacija koristi.
+	 */
 	public VratiListuCasSO(Repository repository) {
 		super(repository);
 	}
@@ -29,12 +48,16 @@ public class VratiListuCasSO extends ApstraktnaSO {
 	protected void izvrsiOperaciju(ApstraktniDomenskiObjekat ado) throws Exception {
 		Cas c = (Cas) ado;
 		casovi = (List<Cas>) repository.getAll(c);
-
 		if (casovi.isEmpty()) {
 			throw new Exception("Casovi nisu vraceni!");
 		}
 	}
 
+	/**
+	 * Vraca listu casova dobijenu izvrsavanjem operacije.
+	 * 
+	 * @return lista casova kao List&lt;Cas&gt;.
+	 */
 	public List<Cas> getCasovi() {
 		return casovi;
 	}
