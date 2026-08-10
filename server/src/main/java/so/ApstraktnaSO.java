9ed892a03a0d2be376897664a1db00fd53eea116
Una Stankovic <unastankovic03@gmail.com>
2026-08-10 15:11:17 +0200

    Dokumentovana klasa ApstraktnaSO JavaDoc-om

--- server/src/main/java/so/ApstraktnaSO.java
+++ server/src/main/java/so/ApstraktnaSO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package so;
 
 import domen.ApstraktniDomenskiObjekat;
@@ -10,49 +6,117 @@ import repository.db.DbRepository;
 import repository.db.impl.DbRepositoryGeneric;
 
 /**
- *
- * @author Una
+ * Predstavlja apstraktnu sistemsku operaciju koja definise zajednicki
+ * tok izvrsavanja svih konkretnih sistemskih operacija u sistemu.
+ * 
+ * Koristi Template Method sablon - metoda izvrsi() definise fiksni
+ * redosled koraka (provera preduslova, otvaranje transakcije, izvrsavanje
+ * operacije, potvrda ili ponistavanje transakcije), dok konkretne
+ * sistemske operacije implementiraju metode preduslov() i izvrsiOperaciju().
+ * 
+ * @author Una Stankovic
+ * @version 1.0
  */
 public abstract class ApstraktnaSO {
-	protected final Repository repository;
 
-	public ApstraktnaSO() {
-		 this.repository = new DbRepositoryGeneric(); 
-	}
+    /**
+     * Repozitorijum preko koga sistemska operacija pristupa bazi podataka.
+     */
+    protected final Repository repository;
 
-	public ApstraktnaSO(Repository repository) {
-		this.repository = repository;
-	}
+    /**
+     * Kreira objekat sistemske operacije sa podrazumevanim repozitorijumom
+     * (DbRepositoryGeneric).
+     */
+    public ApstraktnaSO() {
+        this.repository = new DbRepositoryGeneric();
+    }
 
-	public void izvrsi(ApstraktniDomenskiObjekat ado) throws Exception {
-		try {
-			preduslov(ado);
-			zapocniTransakciju();
-			izvrsiOperaciju(ado);
-			potvrdiTransakciju();
-			System.out.println("Uspesno izvrsena operacija!");
-		} catch (Exception ex) {
-			System.out.println("Neuspesno izvrsena operacija!");
-			ponistiTransakciju();
-			throw ex;
-		} finally {
-			((DbRepository) repository).disconnect();
-		}
-	}
+    /**
+     * Kreira objekat sistemske operacije sa unetim repozitorijumom.
+     * 
+     * Koristi se prvenstveno za testiranje, kada se umesto pravog
+     * repozitorijuma prosledjuje lazni (mock) objekat.
+     * 
+     * @param repository Repozitorijum koji sistemska operacija koristi.
+     */
+    public ApstraktnaSO(Repository repository) {
+        this.repository = repository;
+    }
 
-	protected abstract void preduslov(ApstraktniDomenskiObjekat ado) throws Exception;
+    /**
+     * Izvrsava sistemsku operaciju kroz fiksni redosled koraka.
+     * 
+     * Redom se poziva: provera preduslova, otvaranje transakcije,
+     * izvrsavanje konkretne operacije i potvrda transakcije. Ako u bilo
+     * kom koraku dodje do greske, transakcija se ponistava i greska se
+     * prosledjuje dalje. U svakom slucaju, na kraju se zatvara konekcija
+     * sa bazom.
+     * 
+     * @param ado Domenski objekat nad kojim se izvrsava operacija.
+     * @throws java.lang.Exception ako provera preduslova ne prodje ili
+     * ako dodje do greske prilikom izvrsavanja operacije.
+     */
+    public void izvrsi(ApstraktniDomenskiObjekat ado) throws Exception {
+        try {
+            preduslov(ado);
+            zapocniTransakciju();
+            izvrsiOperaciju(ado);
+            potvrdiTransakciju();
+            System.out.println("Uspesno izvrsena operacija!");
+        } catch (Exception ex) {
+            System.out.println("Neuspesno izvrsena operacija!");
+            ponistiTransakciju();
+            throw ex;
+        } finally {
+            ((DbRepository) repository).disconnect();
+        }
+    }
 
-	protected abstract void izvrsiOperaciju(ApstraktniDomenskiObjekat ado) throws Exception;
+    /**
+     * Proverava da li su ispunjeni preduslovi za izvrsavanje operacije.
+     * 
+     * Konkretne sistemske operacije implementiraju ovu metodu tako da
+     * proveravaju ispravnost prosledjenog domenskog objekta.
+     * 
+     * @param ado Domenski objekat ciji se preduslovi proveravaju.
+     * @throws java.lang.Exception ako preduslovi nisu ispunjeni.
+     */
+    protected abstract void preduslov(ApstraktniDomenskiObjekat ado) throws Exception;
 
-	private void zapocniTransakciju() throws Exception {
-		((DbRepository) repository).connect();
-	}
+    /**
+     * Izvrsava konkretnu operaciju nad prosledjenim domenskim objektom.
+     * 
+     * Konkretne sistemske operacije implementiraju ovu metodu sa svojom
+     * specificnom logikom (dodavanje, izmena, brisanje, pretraga...).
+     * 
+     * @param ado Domenski objekat nad kojim se izvrsava operacija.
+     * @throws java.lang.Exception ako dodje do greske prilikom izvrsavanja
+     * operacije.
+     */
+    protected abstract void izvrsiOperaciju(ApstraktniDomenskiObjekat ado) throws Exception;
 
-	protected void potvrdiTransakciju() throws Exception {
-		((DbRepository) repository).commit();
-	}
+    private void zapocniTransakciju() throws Exception {
+        ((DbRepository) repository).connect();
+    }
 
-	protected void ponistiTransakciju() throws Exception {
-		((DbRepository) repository).rollback();
-	}
+    /**
+     * Potvrdjuje (commit) tekucu transakciju nad bazom podataka.
+     * 
+     * @throws java.lang.Exception ako dodje do greske prilikom potvrde
+     * transakcije.
+     */
+    protected void potvrdiTransakciju() throws Exception {
+        ((DbRepository) repository).commit();
+    }
+
+    /**
+     * Ponistava (rollback) tekucu transakciju nad bazom podataka.
+     * 
+     * @throws java.lang.Exception ako dodje do greske prilikom ponistavanja
+     * transakcije.
+     */
+    protected void ponistiTransakciju() throws Exception {
+        ((DbRepository) repository).rollback();
+    }
 }
