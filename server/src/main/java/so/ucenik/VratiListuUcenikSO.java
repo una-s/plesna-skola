6539ad6e12797149ef95f55729491fe3c03e082d
Una Stankovic <unastankovic03@gmail.com>
2026-08-12 19:09:30 +0200

    Dokumentovana klasa VratiListuUcenikSO JavaDoc-om

--- server/src/main/java/so/ucenik/VratiListuUcenikSO.java
+++ server/src/main/java/so/ucenik/VratiListuUcenikSO.java
@@ -1,36 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package so.ucenik;
 
 import domen.ApstraktniDomenskiObjekat;
 import domen.Ucenik;
 import repository.Repository;
-
 import java.util.List;
 import so.ApstraktnaSO;
-/**
- * 
- * @author Una
- */
 
+/**
+ * Predstavlja sistemsku operaciju kojom se vraca lista svih ucenika
+ * iz sistema.
+ * 
+ * Nasledjuje ApstraktnaSO i implementira preduslov i izvrsiOperaciju.
+ * 
+ * @author Una Stankovic
+ */
 public class VratiListuUcenikSO extends ApstraktnaSO {
 
+    /**
+     * Lista ucenika vracena iz baze podataka.
+     */
     private List<Ucenik> ucenici;
-    
+
+    /**
+     * Kreira objekat sistemske operacije sa podrazumevanim repozitorijumom.
+     */
     public VratiListuUcenikSO() {
         super();
     }
 
+    /**
+     * Kreira objekat sistemske operacije sa unetim repozitorijumom.
+     * 
+     * @param repository Repozitorijum koji sistemska operacija koristi.
+     */
     public VratiListuUcenikSO(Repository repository) {
         super(repository);
     }
 
+    /**
+     * Vraca listu ucenika dobijenu izvrsavanjem operacije.
+     * 
+     * @return lista ucenika kao List&lt;Ucenik&gt;.
+     */
     public List<Ucenik> getUcenici() {
         return ucenici;
     }
 
+    /**
+     * Proverava da li je prosledjeni objekat instanca klase Ucenik.
+     * 
+     * @param ado Domenski objekat ciji se preduslovi proveravaju.
+     * @throws java.lang.Exception ako objekat nije instanca klase Ucenik.
+     */
     @Override
     protected void preduslov(ApstraktniDomenskiObjekat ado) throws Exception {
         if (ado == null || !(ado instanceof Ucenik)) {
@@ -38,6 +59,13 @@ public class VratiListuUcenikSO extends ApstraktnaSO {
         }
     }
 
+    /**
+     * Vraca listu svih ucenika iz baze i cuva je u internoj listi.
+     * 
+     * @param ado Domenski objekat (Ucenik) na osnovu koga se vraca lista.
+     * @throws java.lang.Exception ako dodje do greske prilikom vracanja
+     * ucenika iz baze.
+     */
     @Override
     protected void izvrsiOperaciju(ApstraktniDomenskiObjekat ado) throws Exception {
         Ucenik u = (Ucenik) ado;
